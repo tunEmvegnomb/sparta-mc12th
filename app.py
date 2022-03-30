3e4996f13baa8eb3c34efd492676ee186a63452a
em4715 <70576488+em4715@users.noreply.github.com>
2022-03-30 17:28:00 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import Flask, render_template, jsonify, request, session
 from pymongo import MongoClient
 
 # 클라이언트 정의 - MongoClient를 로컬호스트와 연결
-client = MongoClient('localhost', 27017)
+client = MongoClient('mongodb+srv://making:making@cluster0.ymxju.mongodb.net/Cluster0?retryWrites=true&w=majority')
 
 # 컬렉션 정의. mc12th라는 컬렉션이 생성됨
 db = client.mc12th
