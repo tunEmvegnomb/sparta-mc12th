df055c7990bc44a7975f643e44261f34425d59f1
rhokanson <99600920+rhokanson@users.noreply.github.com>
2022-03-31 22:23:10 +0900

    signup.js first commit

---
+++ static/js/mypage.js
@@ -0,0 +1,15 @@
+console.log(true)
+function showMypage() {
+    $.ajax({
+        type: "GET",
+        url: "/mypage",
+        data: {},
+        success: function (response) {
+            let mypage = response['mypage']
+            let user_id = mypage['user_id']
+            let nickname = mypage['user_nickname']
+            console.log(user_id, nickname)
+        }
+    })
+}
+
---
+++ static/js/signup.js
@@ -0,0 +1,18 @@
+// 회원가입 ajax 코드
+$('.signup-form').on('submit', function(event) {
+    event.preventDefault();
+
+    let user_id = $('#id').val()
+    let user_pwd = $('#password').val()
+    let user_pwd2 = $('#password_check').val()
+    let user_nickname = $('#nickname').val()
+
+    $.ajax({
+        type: 'POST',
+        url: '/signup',
+        data: {id_give: user_id, pwd_give: user_pwd, pwd2_give: user_pwd2, nickname_give: user_nickname}, 
+        success: function (response) {
+            alert(response['msg']);
+        }
+    })
+});
