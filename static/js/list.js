2609fa81140a8effa47ed80becab40db98f3b369
rhokanson <99600920+rhokanson@users.noreply.github.com>
2022-04-06 00:00:38 +0900

    scroll ajax, tasteFilter

--- static/js/list.js
+++ static/js/list.js
@@ -3,6 +3,8 @@ const sortByLike = document.querySelector(".sortByLike")
 
 //최신순 정렬 -> 문제는 검색값, 필터값과 중복 안됨. 현재는 있는 내용을 지우고 다시 불러오는 형태
 $(".sortByDate").on("click", function() {
+  Array.from($(".filter-value")).forEach(value => value.classList.remove("current"))
+
   sortByLike.classList.remove("current")
   sortByDate.classList.add("current")
   $.ajax({
@@ -61,12 +63,15 @@ $(".sortByDate").on("click", function() {
                     }
                 })
         $('.spinner-border').hide();
+        $('#searchInfo').empty();
     }  
 })
 })
 
 //인기순 정렬 -> 문제는 검색값, 필터값과 중복 안됨. 현재는 있는 내용을 지우고 다시 불러오는 형태
 $(".sortByLike").on("click", function() {
+  Array.from($(".filter-value")).forEach(value => value.classList.remove("current"))
+
   sortByDate.classList.remove("current")
   sortByLike.classList.add("current")
   $.ajax({
@@ -125,6 +130,7 @@ $(".sortByLike").on("click", function() {
                     }
                 })
         $('.spinner-border').hide();
+        $('#searchInfo').empty();
     }  
 })
 })
@@ -133,6 +139,9 @@ $(".sortByLike").on("click", function() {
 //검색창 입력(자바스크립트에서 페이크값 내에서 검색되도록 구현함)
 $("form").on("submit", function(event){
   event.preventDefault()
+  Array.from($(".filter-value")).forEach(value => value.classList.remove("current"))
+  sortByLike.classList.remove("current")
+  sortByDate.classList.remove("current")
   const inputVal = $("#searchInput").val()
   $.ajax({
     type: "GET",
@@ -208,18 +217,23 @@ $("form").on("submit", function(event){
 })
 
 
-//필터 선택값 불러오기 
-function onFilterClick(event) {
+//필터 선택값 불러오기 -> TasteFilter만 작동함
+function onTasteFilter(event) {
+  Array.from($(".filter-value")).forEach(value => value.classList.remove("current"))
+  sortByLike.classList.remove("current")
+  sortByDate.classList.remove("current")
+  event.target.classList.add("current")
   const value = event.target.innerText
-  const key = event.target.parentElement.id
-  console.log(key, value)   //recipe_taste, 달달 이런식으로 값 가져옴
+  // const key = event.target.parentElement.id
+  console.log(value)   //recipe_taste, 달달 이런식으로 값 가져옴
   $.ajax({
     type: "GET",
     url: "/list/filter",
-    data: {},
+    data: {taste_give:value, ing_give:null, diff_give:null, time_give:null},
     success: function (response) {
       $('.card-group').empty();
-        let recipes = response['filtered_data']
+      $('.spinner-border').hide();
+        let recipes = response['filtered_data'][0]
         for (let i = 0; i < recipes.length; i++) {
             let name = recipes[i]['recipe_name']
             let img = recipes[i]['recipe_img']
@@ -261,12 +275,185 @@ function onFilterClick(event) {
 })
 }
 
-Array.from($(".filter-value")).forEach(value => value.addEventListener("click", onFilterClick))
+function onIngFilter(event) {
+  Array.from($(".filter-value")).forEach(value => value.classList.remove("current"))
+  sortByLike.classList.remove("current")
+  sortByDate.classList.remove("current")
+  const value = event.target.innerText
+  // const key = event.target.parentElement.id
+  console.log(value)   //recipe_taste, 달달 이런식으로 값 가져옴
+  $.ajax({
+    type: "GET",
+    url: "/list/filter",
+    data: {taste_give:null, ing_give:value, diff_give:null, time_give:null},
+    success: function (response) {
+      $('.card-group').empty();
+      $('.spinner-border').hide();
+        let recipes = response['filtered_data'][0]
+        for (let i = 0; i < recipes.length; i++) {
+            let name = recipes[i]['recipe_name']
+            let img = recipes[i]['recipe_img']
+            let like = recipes[i]['recipe_like']
+            let writter = recipes[i]['recipe_writter']
+            let temp_html = `<div class="col">
+            <div class="card">
+              <div class="card-img-div">
+                  <img
+                    src="${img}"
+                    class="card-img-top"
+                    alt="..."
+                  />
+              </div>
+              <div class="card-body">
+                <div class="card-info">
+                  <a href="#" class="recipe-link"
+                    ><span class="card-title"
+                      >${name}</span
+                    ></a
+                  >
+                  <div class="card-bottom-row">
+                    <span class="card-text">${writter}</span>
+                    <div class="like-div">
+                      <span class="like-text">추천</span>
+                      <span class="like-num">${like}</span>
+                    </div>
+                  </div>
+                </div>
+                <div class="card-like">
+                  <i class="fa-regular fa-thumbs-up like-icon"></i>
+                </div>
+              </div>
+            </div>
+          </div>`
+            $('.card-group').append(temp_html)
+        }
+    }  
+})
+}
+
+function onTimeFilter(event) {
+  Array.from($(".filter-value")).forEach(value => value.classList.remove("current"))
+  sortByLike.classList.remove("current")
+  sortByDate.classList.remove("current")
+  const value = event.target.innerText
+  // const key = event.target.parentElement.id
+  console.log(value)   //recipe_taste, 달달 이런식으로 값 가져옴
+  $.ajax({
+    type: "GET",
+    url: "/list/filter",
+    data: {taste_give:null, ing_give:null, diff_give:null, time_give:value},
+    success: function (response) {
+      $('.card-group').empty();
+      $('.spinner-border').hide();
+        let recipes = response['filtered_data'][0]
+        for (let i = 0; i < recipes.length; i++) {
+            let name = recipes[i]['recipe_name']
+            let img = recipes[i]['recipe_img']
+            let like = recipes[i]['recipe_like']
+            let writter = recipes[i]['recipe_writter']
+            let temp_html = `<div class="col">
+            <div class="card">
+              <div class="card-img-div">
+                  <img
+                    src="${img}"
+                    class="card-img-top"
+                    alt="..."
+                  />
+              </div>
+              <div class="card-body">
+                <div class="card-info">
+                  <a href="#" class="recipe-link"
+                    ><span class="card-title"
+                      >${name}</span
+                    ></a
+                  >
+                  <div class="card-bottom-row">
+                    <span class="card-text">${writter}</span>
+                    <div class="like-div">
+                      <span class="like-text">추천</span>
+                      <span class="like-num">${like}</span>
+                    </div>
+                  </div>
+                </div>
+                <div class="card-like">
+                  <i class="fa-regular fa-thumbs-up like-icon"></i>
+                </div>
+              </div>
+            </div>
+          </div>`
+            $('.card-group').append(temp_html)
+        }
+    }  
+})
+}
+
+function onDiffFilter(event) {
+  Array.from($(".filter-value")).forEach(value => value.classList.remove("current"))
+  sortByLike.classList.remove("current")
+  sortByDate.classList.remove("current")
+  const value = event.target.innerText
+  // const key = event.target.parentElement.id
+  console.log(value)   //recipe_taste, 달달 이런식으로 값 가져옴
+  $.ajax({
+    type: "GET",
+    url: "/list/filter",
+    data: {taste_give:null, ing_give:null, diff_give:value, time_give:null},
+    success: function (response) {
+      $('.card-group').empty();
+      $('.spinner-border').hide();
+        let recipes = response['filtered_data'][0]
+        for (let i = 0; i < recipes.length; i++) {
+            let name = recipes[i]['recipe_name']
+            let img = recipes[i]['recipe_img']
+            let like = recipes[i]['recipe_like']
+            let writter = recipes[i]['recipe_writter']
+            let temp_html = `<div class="col">
+            <div class="card">
+              <div class="card-img-div">
+                  <img
+                    src="${img}"
+                    class="card-img-top"
+                    alt="..."
+                  />
+              </div>
+              <div class="card-body">
+                <div class="card-info">
+                  <a href="#" class="recipe-link"
+                    ><span class="card-title"
+                      >${name}</span
+                    ></a
+                  >
+                  <div class="card-bottom-row">
+                    <span class="card-text">${writter}</span>
+                    <div class="like-div">
+                      <span class="like-text">추천</span>
+                      <span class="like-num">${like}</span>
+                    </div>
+                  </div>
+                </div>
+                <div class="card-like">
+                  <i class="fa-regular fa-thumbs-up like-icon"></i>
+                </div>
+              </div>
+            </div>
+          </div>`
+            $('.card-group').append(temp_html)
+        }
+    }  
+})
+}
+
+Array.from($("#recipe_taste .filter-value")).forEach(value => value.addEventListener("click", onTasteFilter))
+Array.from($("#recipe_ing .filter-value")).forEach(value => value.addEventListener("click", onIngFilter))
+Array.from($("#recipe_time .filter-value")).forEach(value => value.addEventListener("click", onTimeFilter))
+Array.from($("#recipe_diff .filter-value")).forEach(value => value.addEventListener("click", onDiffFilter))
 
 
 //처음 데이터 출력
 $(document).ready(function() {
+  $('.d-flex').hide()
   showRecipes();
+  setTimeout(loadMore, 2000)
 })
 
 //처음 데이터 출력
@@ -274,9 +461,9 @@ function showRecipes() {
     $.ajax({
         type: "GET",
         url: "/list/data",
-        data: {},
+        data: {scroll_give:"off"},
         success: function (response) {
-            let recipes = response['append_data']
+            let recipes = response['append_data'][0]
             for (let i = 0; i < recipes.length; i++) {
                 let name = recipes[i]['recipe_name']
                 let img = recipes[i]['recipe_img']
@@ -313,6 +500,8 @@ function showRecipes() {
                 </div>
               </div>`
                 $('.card-group').append(temp_html)
+                
+                
 
                 let recipe_name = "";
                 $('.card-title').on('click', function () {
@@ -326,33 +515,88 @@ function showRecipes() {
                     }
                 })
 
-            }          
+            }
+          $('.d-flex').show()
         }  
     })
 
 }
 
-
-//레시피 이름으로 출력
-
-
-
-
-
+//스크롤 내릴때 더 불러옴
+function loadMore() {
  // 스크롤이 맨 밑까지 가면 실행 되는 조건문
 const target = document.querySelector(".spinner-border")
 const option = {
-  rootMargin: '100px'
+  rootMargin: '0px'
 }
 const callback = (entry, observer) => {
   if(entry[0].isIntersecting) { 
-    console.log("load more!")  //여기에 다음 데이터 추가하는 함수 쓰기
+    console.log("load more!")
+    $('.d-flex').empty()  
+    $.ajax({
+      type: "GET",
+      url: "/list/data",
+      data: {scroll_give:"on"},
+      success: function (response) {
+          let recipes = response['append_data'][0]
+          for (let i = 0; i < recipes.length; i++) {
+              let name = recipes[i]['recipe_name']
+              let img = recipes[i]['recipe_img']
+              let like = recipes[i]['recipe_like']
+              let writter = recipes[i]['recipe_writter']
+              let temp_html = `<div class="col">
+              <div class="card">
+                <div class="card-img-div">
+                    <img
+                      src="${img}"
+                      class="card-img-top"
+                      alt="..."
+                    />
+                </div>
+                <div class="card-body">
+                  <div class="card-info">
+                    <a href="#" class="recipe-link"
+                      ><span class="card-title"
+                        >${name}</span
+                      ></a
+                    >
+                    <div class="card-bottom-row">
+                      <span class="card-text">${writter}</span>
+                      <div class="like-div">
+                        <span class="like-text">추천</span>
+                        <span class="like-num">${like}</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="card-like">
+                    <i class="fa-regular fa-thumbs-up like-icon"></i>
+                  </div>
+                </div>
+              </div>
+            </div>`
+              $('.card-group').append(temp_html)
+
+              let recipe_name = "";
+              $('.card-title').on('click', function () {
+                  recipe_name = $(this).text()
+                  console.log(recipe_name)
+              })
+
+              $(document).on('click', '.card-title', function () {
+                  if (recipe_name != null) {
+                      document.location.href = `/detail?recipe_name=${recipe_name}`;
+                  }
+              })
+
+          }     
+             
+      }  
+  })
   } 
 }
 const observer = new IntersectionObserver(callback, option);
-observer.observe(target) //여기까지 스크롤 그거
-
-
+observer.observe(target)
+}
 
 // //좋아요 처음 누르면 +1, 다시 누르면 -1
 // const likeIcon = document.querySelector(".like-icon")
